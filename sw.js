2655105ad7562b902852a69e4dae09e4865503b7
Lucas Rocha <lucas@3v3.com.br>
2022-09-29 14:13:05 -0300

    jQuery + Bulma

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this to your repository name
-var GHPATH = '/cadastramento';
+var PATH = '/cadastramento';
 
 // Choose a different app prefix name
 var APP_PREFIX = '3v3cad';
@@ -8,13 +8,14 @@ var APP_PREFIX = '3v3cad';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_00';
+var VERSION = 'v0.0.1';
 
 // The files to make available for offline use. make sure to add 
 // others to this list
 var URLS = [    
-  `${GHPATH}/`,
-  `${GHPATH}/index.html`,
-  `${GHPATH}/css/styles.css`,
-  `${GHPATH}/js/app.js`
+  `${PATH}/`,
+  `${PATH}/index.html`,
+  `${PATH}/css/bulma.min.css`,
+  `${PATH}/js/jquery-3.6.1.min.js`,
+  `${PATH}/js/app.js`
 ]

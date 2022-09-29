fe6fb8b12c64277e1997dcd1d5c1d36d848fc6d3
Lucas Rocha <lucas@3v3.com.br>
2022-09-29 16:21:40 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ var APP_PREFIX = '3v3cad';
 var VERSION = 'v202209291549';
 var CACHE_NAME = APP_PREFIX + VERSION;
 var URLS = [    
- /*  `${PATH}/`, */
+  `${PATH}/`,
   `${PATH}/index.html`,
   `${PATH}/css/bulma.min.css`,
   `${PATH}/js/jquery-3.6.1.min.js`,
@@ -34,5 +34,22 @@ self.addEventListener('fetch', event => {
 			}
 		})
 	)
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      var cacheWhitelist = keyList.filter(key => {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map((key, i) => {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('Apagando o cache: ' + keyList[i] );
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
 })
 

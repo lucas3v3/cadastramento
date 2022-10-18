acddb0861f7c2f8a4aab4c37c228fa2e3d2e995c
Lucas Rocha <lucas@3v3.com.br>
2022-10-18 09:12:17 -0300

    Download test

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
-if (navigator.serviceWorker) {
+/* if (navigator.serviceWorker) {
     navigator.serviceWorker.register (
         '/cadastramento/sw.js',
         { scope: '/cadastramento/' }
     ).then(() => console.log("Service Worker registrado"));
+} */
+
+const exportJSON = el => {
+	const response = {
+		"select": $("#exampleFormControlSelect1").val()
+	}
+	const response_json = encodeURIComponent(JSON.stringify(response)); 
+	const data = `text/json;charset=utf-8,${response_json}`;
+	console.log(JSON.stringify(response));
+	el.setAttribute("href", `data:${data}`);
+	el.setAttribute("download", "data.json");
 }

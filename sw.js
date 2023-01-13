4539b5634f65f105ec099d8233787530d84c7806
lucas3v3 <lucas@3v3.com.br>
2023-01-13 15:50:31 -0300

    Pequenos ajustes nas tabelas

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const PATH = "/cadastramento";
 const APP_PREFIX = "3V3-CAD";
-const VERSION = "v20230106.2";
+const VERSION = "v20230113.1";
 const CACHE_NAME = APP_PREFIX + VERSION;
 const ASSETS = [
   `${PATH}/manifest.webmanifest`,
